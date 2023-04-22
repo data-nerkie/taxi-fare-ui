a7346b6318283e89891d396d8234df552d78b467
TeKa <kiener.thomas@gmail.com>
2023-04-22 10:29:20 +0200

    add script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+// let taxiFareApiUrl = 'http://localhost:8000/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // alternatively, you may use this Le Wagon Prediction API if you do not have one in production
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoidGVreS10ZWthIiwiYSI6ImNsN2s4bzMzejA0Y3EzdnA5ZmwxOXVlcnMifQ.j_ULItc43M3WMIRgSuKdkw';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
